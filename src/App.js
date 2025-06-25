9ead433f4a18c233b405cf675dec423ce553e3fd
seema0210 <seemadnale02@gmail.com>
2025-06-25 15:45:45 +0530

    api integation with ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import NewCard from './NewCard';
 
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [openModal, setOpenModal] = useState(false)
+
+  const getAllCards = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/users", {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch users")
+      }
+
+      const getRes = await response.json()
+      const finalUsers = getRes.users.slice(0, 9)
+      return finalUsers;
+    } catch (error) {
+      console.log('fetch api error', error)
+    }
+  }
+
+  const handleDelete = (currentId) => {
+    try {
+      fetch(`https://dummyjson.com/users/${currentId}`, {
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+
+      setCards((prevCard) => prevCard?.filter((item) => item.id !== currentId))
+
+    } catch (error) {
+      console.log('delete api error', error);
+    }
+  }
+
+  const handleAdd = () => {
+    setOpenModal(true)
+  }
+
+  useEffect(() => {
+    getAllCards().then((data) => setCards(data))
+  }, [])
+
   return (
-    <div>
-      Add Files
+    <div className="container">
+      <div className="button-container">
+        <button className="add-btn" onClick={handleAdd}>
+          Add User Card +
+        </button>
+      </div>
+      <div className="card-grid">
+        {cards.map((item) => (
+          <div className="card" key={item.id}>
+            <h3>{item.firstName} {item.lastName}</h3>
+            <p>{item.email}</p>
+            <div className="card-footer">
+              <p>{item.role}</p>
+              <button type='button' onClick={() => handleDelete(item.id)}>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      {
+        openModal && (
+          <div className="modal-overlay">
+            <NewCard
+            setOpenModal={setOpenModal}
+            setCards={setCards}
+            />
+          </div>
+        )
+      }
     </div>
+
   );
 }
 
---
+++ src/NewCard.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react'
+
+const NewCard = ({ setOpenModal, setCards }) => {
+
+    const initialState = {
+        fname: '',
+        lname: '',
+        email: '',
+        role: ''
+    }
+
+    const [newUser, setNewUer] = useState(initialState)
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setNewUer((preUser) => ({ ...preUser, [name]: value }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const { fname, lname, email, role } = newUser
+
+        const finalUser = {
+            id: Math.random(),
+            firstName: fname,
+            lastName: lname,
+            email: email,
+            role: role
+        }
+        const cretedUser = [finalUser]
+        setCards((preData) => {
+            return [...preData, ...cretedUser]
+        })
+        setOpenModal(false)
+    }
+
+    return (
+        <div className="modal-box">
+            <h2>Add New User</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                    name='fname'
+                    placeholder='Enter First Name'
+                    value={newUser.fname}
+                    onChange={handleChange}
+                    required /> <br />
+                <input type="text"
+                    name='lname'
+                    placeholder='Enter Last Name'
+                    value={newUser.lname}
+                    onChange={handleChange}
+                    required /> <br />
+                <input type="email"
+                    name='email'
+                    placeholder='Enter Email'
+                    value={newUser.email}
+                    onChange={handleChange}
+                    required /> <br />
+                <input type="text"
+                    name='role'
+                    placeholder='Enter Role'
+                    value={newUser.role}
+                    onChange={handleChange}
+                    required />
+                <div className="modal-buttons">
+                    <button type='button' onClick={() => setOpenModal(false)}>Close</button>
+                    <button type='submit'>Add</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default NewCard
